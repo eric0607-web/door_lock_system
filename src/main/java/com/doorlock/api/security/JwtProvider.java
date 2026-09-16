86dd08e403f59d2e102a796009a3f6f37ef8765f
eric0607-web <pop5805pop@gmail.com>
2026-09-16 21:20:41 +0900

    feat: JWT 의존성 추가 및 JWT 인증 설정

---
+++ src/main/java/com/doorlock/api/security/JwtProvider.java
@@ -0,0 +1,48 @@
+package com.doorlock.api.security;
+
+import com.doorlock.api.domain.Role;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    private final SecretKey key;
+    private final long expirationMs;
+
+    public JwtProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration-ms}") long expirationMs
+    ) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+        this.expirationMs = expirationMs;
+    }
+
+    public String generateToken (Long userId, String email, Role role){
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + expirationMs);
+
+        return Jwts.builder()
+                .subject(email)
+                .claim("userId", userId)
+                .claim("role", role.name())
+                .issuedAt(now)
+                .expiration(expiry)
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+}
